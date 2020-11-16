258963bee1b5ba64ddabfef473f4bcfeca9d01eb
oluwadara <daraoloye99@gmail.com>
2020-11-16 21:18:55 +0100

    Consumed the API from AdonisJS deployment

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import  routes from './routes'
+// import VueAxios from './plugins/axios'
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+// Vue.use(VueAxios)
+
+// export const axios = axios
 
 const router = new VueRouter({
 	routes
